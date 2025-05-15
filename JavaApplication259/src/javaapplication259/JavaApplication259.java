ed47cfb4fb6191de3c8bce4bc176b508bcac0ed5
henriqueortiz975 <henriqueortiz3090@gmail.com>
2025-05-15 17:17:10 -0300

    exercicios 1(finalizado)

--- JavaApplication259/src/javaapplication259/JavaApplication259.java
+++ JavaApplication259/src/javaapplication259/JavaApplication259.java
@@ -1,21 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaapplication259;
 
-/**
- *
- * @author aluno.saolucas
- */
 public class JavaApplication259 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int n1 = 1;
+        int n2 = 2;
+        int n3 = 3;
+        int n4 = 6;
+        boolean re = Exercicio1(n1, true, n2, n3, n4);
+        System.out.println(re);
     }
-    
+
+    public static boolean Exercicio1(int n1, boolean re, int n2, int n3, int n4) {
+        return n1 + n2 + n3 == n4;
+
+    }
+
 }
