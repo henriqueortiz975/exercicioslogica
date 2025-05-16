b4d9bc33bfef0356d779bb36967970d5b5255e54
henriqueortiz975 <henriqueortiz3090@gmail.com>
2025-05-16 17:41:46 -0300

    add exercicio03

---
+++ exercicio03/src/exercicio03/Exercicio03.java
@@ -0,0 +1,12 @@
+package exercicio03;
+
+public class Exercicio03 {
+
+    public static void main(String[] args) {
+        for(int i = 1; i <= 3; i++){
+            System.out.println("valor de i: " + i);
+        }
+
+    }
+
+}
